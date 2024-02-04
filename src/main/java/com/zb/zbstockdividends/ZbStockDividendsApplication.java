a664a6b6e55f0648e7f171ccd46b5a3f913b708d
je-pa <jieun0502@gmail.com>
2024-02-04 23:54:23 +0900

    Initial commit

---
+++ src/main/java/com/zb/zbstockdividends/ZbStockDividendsApplication.java
@@ -0,0 +1,13 @@
+package com.zb.zbstockdividends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbStockDividendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbStockDividendsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zb/zbstockdividends/ZbStockDividendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.zbstockdividends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbStockDividendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
