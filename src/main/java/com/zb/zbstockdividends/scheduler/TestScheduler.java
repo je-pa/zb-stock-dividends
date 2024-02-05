f799c384c7615455e801e9062d700474ae63a483
je-pa <jieun0502@gmail.com>
2024-02-06 01:59:39 +0900

    스케쥴러 테스트

---
+++ src/main/java/com/zb/zbstockdividends/scheduler/TestScheduler.java
@@ -0,0 +1,12 @@
+package com.zb.zbstockdividends.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestScheduler {
+    @Scheduled(cron = "0/5 * * * * *")
+    public void test(){
+        System.out.println("now ->" + System.currentTimeMillis());
+    }
+}
