fe6655aeaacdae5107770cd0622614299a5c689e
je-pa <jieun0502@gmail.com>
2024-02-05 11:11:33 +0900

    jsoup 테스트

--- src/main/java/com/zb/zbstockdividends/ZbStockDividendsApplication.java
+++ src/main/java/com/zb/zbstockdividends/ZbStockDividendsApplication.java
@@ -1,13 +1,46 @@
 package com.zb.zbstockdividends;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ZbStockDividendsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZbStockDividendsApplication.class, args);
+//        SpringApplication.run(ZbStockDividendsApplication.class, args);
+        // jsoup.org/apidocs
+        Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1707004800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+        try {
+            Document document = connection.get();
+            Elements elements = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element element = elements.get(0);
+            Element tbody = element.children().get(1);
+
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                System.out.println(year + "/" + month + "/" + day + "->" + dividend);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
